d6f4ea19ba654279418110cd42502f64807f8442
Kuba Nowak <kubanowak9755@gmail.com>
2025-05-27 17:37:14 +0200

    Add recipe_likes association to the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,12 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100, minimum: 3 }
   validates :text, presence: true, length: { maximum: 1000, minimum: 10 }
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :featured, inclusion: { in: [true, false] }
 
   enum difficulty: { easy: 0, medium: 1, hard: 2, expert: 3 }
 
   belongs_to :author
   has_many :recipe_categories, dependent: :destroy
   has_many :categories, through: :recipe_categories
+  has_many :recipe_likes, dependent: :destroy
 end
