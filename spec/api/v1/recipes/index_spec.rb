37150fc95e8e52017db32e62c469fff4f63af4ba
Kuba Nowak <kubanowak9755@gmail.com>
2025-05-27 17:55:42 +0200

    Update recipes index spec to validate fetching likes_count and filtering recipes liked by user

--- spec/api/v1/recipes/index_spec.rb
+++ spec/api/v1/recipes/index_spec.rb
@@ -19,4 +19,50 @@ RSpec.describe "recipes#index", type: :request do
       expect(d.map(&:id)).to match_array([recipe1.id, recipe2.id])
     end
   end
+
+  describe 'fetch recipes with likes_counts' do
+    let!(:recipe1) { create(:recipe) }
+    let!(:recipe2) { create(:recipe) }
+    let!(:like1) { create(:recipe_like, recipe: recipe1) }
+    let!(:like2) { create(:recipe_like, recipe: recipe1) }
+    let!(:like3) { create(:recipe_like, recipe: recipe2) }
+
+    before do
+      params[:fields] = { recipes: 'likes_count' }
+    end
+
+    it 'returns recipes with likes_count' do
+      expect(RecipeResource).to receive(:all).and_call_original
+
+      make_request
+
+      expect(response.status).to eq(200), response.body
+      expect(d.map(&:jsonapi_type).uniq).to match_array(['recipes'])
+      expect(d.map(&:id)).to match_array([recipe1.id, recipe2.id])
+      expect(d.map(&:attributes).map { |a| a['likes_count'] }).to eq([2, 1])
+    end
+  end
+
+  describe 'fetch only recipes liked by user' do
+    let(:user) { create(:user) }
+    let!(:recipe1) { create(:recipe) }
+    let!(:recipe2) { create(:recipe) }
+    let!(:recipe3) { create(:recipe) }
+    let!(:like1) { create(:recipe_like, user: user, recipe: recipe1) }
+    let!(:like2) { create(:recipe_like, user: user, recipe: recipe2) }
+
+    before do
+      params[:filter] = { liked_by_user_id: { eq: user.id } }
+    end
+
+    it 'returns only recipes liked by the user' do
+      expect(RecipeResource).to receive(:all).and_call_original
+
+      make_request
+
+      expect(response.status).to eq(200), response.body
+      expect(d.map(&:jsonapi_type).uniq).to match_array(['recipes'])
+      expect(d.map(&:id)).to match_array([recipe1.id, recipe2.id])
+    end
+  end
 end
